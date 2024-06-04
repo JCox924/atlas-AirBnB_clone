8583cdedbfd0266ba552159c369a6a583124d7a6
JCox924 <Joshua.Cox@atlasschool.com>
2024-06-04 08:57:47 -0500

    simple console built

---
+++ console.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+import cmd
+
+class HBNBCommand(cmd.Cmd):
+    """Command interpreter for the AirBnB clone project"""
+    prompt = '(hbnb) '
+
+    def do_quit(self, arg):
+        """Quit command to exit the program"""
+        return True
+
+    def do_EOF(self, arg):
+        """EOF command to exit the program"""
+        return True
+
+    def emptyline(self):
+        """Do nothing on empty line + ENTER"""
+        pass
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
